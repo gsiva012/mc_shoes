e1cdce3660803c4e56c08117d721b9219c3579fc
gsiva012 <ganasiva012@gmail.com>
2025-03-31 13:07:21 +0530

    Add herder and footer

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "mc_shoes"
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Feel your every step"
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A Shoe ecommerce store built with Next.js and MongoDB"
